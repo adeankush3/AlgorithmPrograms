7178fba5431208d396b27f6b6e028c9e11da8464
adeankush3 <adeankush3@gmail.com>
2022-02-15 21:25:31 +0530

    [Ankush]Prime Number Program Complete

--- AlgorithmPrograms/AlgorithmPrograms/BinarySearch.cs
+++ AlgorithmPrograms/AlgorithmPrograms/BinarySearch.cs
@@ -80,5 +80,29 @@ namespace AlgorithmPrograms
                 Console.WriteLine("String Are Not Angram");
             }
         }
+        public void PrimeNumber()
+        {
+            bool isPrime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 1000; i++)
+            {
+                for (int j = 2; j <= 1000; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
+            Console.ReadKey();
+        }
     }
 }
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -12,14 +12,37 @@ namespace AlgorithmPrograms
         {
             Console.WriteLine("Welcome To Algorithm Program");
 
-            BinarySearch binarySearch = new BinarySearch();
-            Console.WriteLine("Enter string value to search:");
-            string userInput = Console.ReadLine();
-            Console.WriteLine("User input is :" + userInput);
-            binarySearch.BinarySearchIterative(userInput);
+            
+            Console.WriteLine("Welcome to Algorithm Programs :  \n Choose operation :" +
+            "\n 1. Binary search" +
+            "\n 2. Angram" +
+            "\n 3. PrimeNumber"+
+            "\n 3. Exit");
+
+            int option = Convert.ToInt32(Console.ReadLine());
 
 
-            binarySearch.Angram();
+            switch (option)
+            {
+                case 1:
+                    BinarySearch binarySearch = new BinarySearch();
+                    Console.WriteLine("Enter string value to search:");
+                    string userInput = Console.ReadLine();
+                    Console.WriteLine("User input is :" + userInput);
+                    binarySearch.BinarySearchIterative(userInput);
+                    break;
+                case 2:
+                    BinarySearch binarySearch1 = new BinarySearch();
+                    binarySearch1.Angram();
+                    break;
+                case 3:
+                    BinarySearch binSearch2 = new BinarySearch();
+                    binSearch2.PrimeNumber();
+                    break;
+                default:
+                    System.Console.WriteLine("Choose correct option");
+                    break;
+            }
         }
     }
 }
